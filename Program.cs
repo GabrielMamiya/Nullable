6ec1e14074aee1fb5d18c08555d820a795482ee5
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-03-15 23:12:30 -0300

    Initial check-in of module NullableInt

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NullableInt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int teste = null;
+
+            Nullable<int> testando = null;
+            //ou
+            int? testando1 = null;
+        }
+    }
+}
